6eb8ea7774fddeaa7811dceaddf99c40fe647c04
derrickric <derrickmbaka@gmail.com>
2022-07-01 20:02:02 -0700

    to where you will go

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry poimt
+ * Return: Always 0
+ */
+{
+char letter;
+
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar(letter);
+putchar('\n');
+return (0);
+}
