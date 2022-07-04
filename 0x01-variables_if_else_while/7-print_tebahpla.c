675ce2efb0977290032e3c0a4a03c60cc31208b4
derrickric <derrickmbaka@gmail.com>
2022-07-03 17:03:27 -0700

    aah

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Entry poimt
+ * main - Entry point
  * Return: Always 0
  */
+int main(void)
 {
-char letter;
+	int num;
+        char letter;
 
-for (letter = 'z'; letter >= 'a'; letter--)
-putchar(letter);
-putchar('\n');
-return (0);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
 }
