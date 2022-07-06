e6689a1143775e02ced3c564c8082679d9cbdfba
derrickric <derrickmbaka@gmail.com>
2022-07-06 14:09:07 -0700

    printing fibanocci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int x, y, n;
+	int i = 0;
+
+	x = 1, y = 2;
+
+	printf("%ld, ", x);
+	printf("%ld", y);
+	for ( ; i < 48; ++i)
+
+	{
+		n = x + y;
+		printf(", ");
+		printf("%ld", n);
+
+		x = y;
+		y = n;
+		n = x + y;
+		if ((i == 47))
+			printf("\n");
+	}
+	return (0);
+}
