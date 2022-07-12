5e60e305415889675a949fd2fcc15e2fc16377d4
derrickric <derrickmbaka@gmail.com>
2022-07-12 03:04:21 -0700

    correction

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: the string
+ * Return: the length of the string
+ */
+void _puts(char *str)
+{
+	int y = 0;
+	while (str[y] != '\0')
+{
+	_putchar(str[y]);
+	y++;
+}
+_putchar('\n');
+}
