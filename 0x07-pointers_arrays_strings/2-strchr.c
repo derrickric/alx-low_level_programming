0249c6dd3832001cb0a42158d3ba297ea7e4436f
derrickric <derrickmbaka@gmail.com>
2022-07-26 01:48:47 -0700

    first phase of 0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - copies memory area.
+ * @s: string to search
+ * @c: char to search for
+ *
+ * Return: s or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+
+}
